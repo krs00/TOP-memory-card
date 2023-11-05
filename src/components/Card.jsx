455d2563f6401a0055916cb73500558f7028e802
Kymani Stephens <kymanist00@gmail.com>
2023-11-05 13:50:51 -0800

    add working api call to grab images

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,36 @@
+import React, { useState, useEffect } from 'react';
+
+
 function Card() {
+
+  const [imageData, setImageData] = useState(null)
+
+  useEffect(() => { 
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data)
+        const imageURL = data.message;
+        setImageData(imageURL);
+      })
+      .catch((error) => {
+        console.error('Error fetching image:', error);
+      });
+  }, []);
+
+
+
 
     return (
       <>
-      
+      <div>
+        <img src={imageData} alt="" />
+      </div>
       </> 
     )
   }
