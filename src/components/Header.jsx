1be86309cb79c3d0cfca31f3f4067b3a109e743c
Kymani Stephens <kymanist00@gmail.com>
2023-11-05 13:51:14 -0800

    add basic header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,8 @@ function Header() {
 
     return (
       <>
-      
+      <h1> ~ Memory Game ~</h1>
+      <h2>Get points by clicking on an image but don't click on any image more than once!</h2>
       </> 
     )
   }
